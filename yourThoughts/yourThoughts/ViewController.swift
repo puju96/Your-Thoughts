da28ab5f2b1f527b1d38f5bede7f793b784f1c4c
Kumar Chikkanna <kumar.c@xlbyte.com>
2018-12-04 11:24:33 +0530

    xcode project created

---
+++ yourThoughts/yourThoughts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yourThoughts
+//
+//  Created by Apple on 04/12/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
